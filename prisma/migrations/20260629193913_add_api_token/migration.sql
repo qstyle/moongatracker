807e76377ae9a713aecc5ecfa4d3d0425ab31edb
Ордин Дмитрий <ordin404@gmail.com>
2026-06-29 22:39:17 +0300

    feat(db): add ApiToken model

---
+++ prisma/migrations/20260629193913_add_api_token/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "ApiToken" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "tokenHash" TEXT NOT NULL,
+    "scope" TEXT[],
+    "lastUsedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ApiToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ApiToken_tokenHash_key" ON "ApiToken"("tokenHash");
